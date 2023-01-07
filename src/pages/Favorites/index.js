b9dc23f81a0cd250778994312c51814f990aa3df
OtavioFSantos <otaviofs01@gmail.com>
2023-01-07 20:56:01 -0300

    added liked movies to favorites page

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,15 +1,22 @@
 import Banner from "components/Banner";
 import Title from "components/Title";
+import Card from "components/Card";
+import { useFavoriteContext } from "Context/Favorites";
 import styles from "./Favorites.module.css";
 
 export default function Favorites() {
+  const { favorite } = useFavoriteContext();
   return (
     <>
       <Banner image="favorites" />
       <Title>
         <h1>These are the movies you liked!</h1>
       </Title>
-      <section className={styles.container}></section>
+      <section className={styles.container}>
+        {favorite.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
+      </section>
     </>
   );
 }
