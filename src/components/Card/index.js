285eb42d99b295fc35eb95c52fc5f5eefba1c454
OtavioFSantos <otaviofs01@gmail.com>
2023-01-07 20:54:30 -0300

    added like and dislike to card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,25 @@
+import { useFavoriteContext } from "Context/Favorites";
 import styles from "./Card.module.css";
 import like from "./like.png";
+import dislike from "./dislike.png";
 
 export default function Card({ id, title, image }) {
+  const { favorite, addFavorite } = useFavoriteContext();
+  const isFavorite = favorite.some((fav) => fav.id === id);
+  const icon = isFavorite ? dislike : like;
+
   return (
     <div className={styles.container}>
       <img src={image} alt={title} className={styles.image} />
       <h2>{title}</h2>
-      <img src={like} alt="Like movie" className={styles.like} />
+      <img
+        src={icon}
+        alt="Like movie"
+        className={styles.like}
+        onClick={() => {
+          addFavorite({ id, title, image });
+        }}
+      />
     </div>
   );
 }
