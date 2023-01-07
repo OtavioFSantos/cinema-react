fcd5379d109d806af6caabfc0c0ca24717ed6da2
OtavioFSantos <otaviofs01@gmail.com>
2023-01-07 11:32:31 -0300

    added components and display video cards

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,21 @@
-import Header from "components/Header";
-import Footer from "components/Footer";
+import Banner from "components/Banner";
+import Title from "components/Title";
+import Card from "components/Card";
+import videos from "json/db.json";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Footer />
+      <Banner image="home" />
+      <Title>
+        <h1>Select the movies you would like to watch!</h1>
+      </Title>
+      <section className={styles.container}>
+        {videos.map((video) => {
+          return <Card {...video} key={video.id} />;
+        })}
+      </section>
     </>
   );
 }
