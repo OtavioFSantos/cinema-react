640bbe1f8df88d39fb70e7a0b03945816e4eefb0
OtavioFSantos <otaviofs01@gmail.com>
2023-01-07 20:56:19 -0300

    added the favorites provider

--- src/routes.js
+++ src/routes.js
@@ -4,16 +4,19 @@ import Favorites from "pages/Favorites";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import Container from "components/Container";
+import FavoritesProvider from "Context/Favorites";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Header />
       <Container>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/favorites" element={<Favorites />}></Route>
-        </Routes>
+        <FavoritesProvider>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/favorites" element={<Favorites />}></Route>
+          </Routes>
+        </FavoritesProvider>
       </Container>
       <Footer />
     </BrowserRouter>
