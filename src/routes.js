6f89acd347da7e4dcc1b90c66f6513471ff67b9e
OtavioFSantos <otaviofs01@gmail.com>
2023-01-07 11:22:10 -0300

    added favorites route and used components

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Favorites from "pages/Favorites";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import Container from "components/Container";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/favorites" element={<Favorites />}></Route>
+        </Routes>
+      </Container>
+      <Footer />
     </BrowserRouter>
   );
 }
