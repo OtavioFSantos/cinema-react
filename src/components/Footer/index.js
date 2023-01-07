3d6c66a7eab79fb272d761da10dc04a96471d34c
OtavioFSantos <otaviofs01@gmail.com>
2023-01-06 21:17:19 -0300

    footer added

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function footer() {
+  return (
+    <footer className={styles.footer}>
+      <h2>Developed by @otaviofsantos</h2>
+    </footer>
+  );
+}
