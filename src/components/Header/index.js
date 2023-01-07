ed6476828eeed866798bead85ec18bccbd228d19
OtavioFSantos <otaviofs01@gmail.com>
2023-01-06 21:16:00 -0300

    create header

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
+import styles from "./Header.module.css";
+import HeaderLink from "components/HeaderLink";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <Link to="./">
+        <img src={logo} alt="cinema logo" className={styles.img}></img>
+      </Link>
+      <nav>
+        <HeaderLink url="./">Home</HeaderLink>
+        <HeaderLink url="./favorites">Favorites</HeaderLink>
+      </nav>
+    </header>
+  );
+}
