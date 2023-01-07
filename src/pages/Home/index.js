259be5bc790b41e138afb2ed4ec9709878278108
OtavioFSantos <otaviofs01@gmail.com>
2023-01-07 11:34:11 -0300

    minor change

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import Banner from "components/Banner";
 import Title from "components/Title";
 import Card from "components/Card";
-import videos from "json/db.json";
+import movies from "json/db.json";
 import styles from "./Home.module.css";
 
 export default function Home() {
@@ -12,8 +12,8 @@ export default function Home() {
         <h1>Select the movies you would like to watch!</h1>
       </Title>
       <section className={styles.container}>
-        {videos.map((video) => {
-          return <Card {...video} key={video.id} />;
+        {movies.map((movie) => {
+          return <Card {...movie} key={movie.id} />;
         })}
       </section>
     </>
