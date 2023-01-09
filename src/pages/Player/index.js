fc568e371bfeed0f4e074e12a17b657976cd3de2
OtavioFSantos <otaviofs01@gmail.com>
2023-01-08 23:43:18 -0300

    add video player page

---
+++ src/pages/Player/index.js
@@ -0,0 +1,33 @@
+import Banner from "components/Banner";
+import Title from "components/Title";
+import { useParams } from "react-router-dom";
+import videos from "json/db.json";
+import styles from "./Player.module.css";
+
+export default function Player() {
+  const params = useParams();
+  const video = videos.find((video) => {
+    return video.id === Number(params.id);
+  });
+
+  return (
+    <>
+      <Banner image="player" />
+      <Title>
+        <h1>Watch {video.title} bellow!</h1>
+      </Title>
+
+      <section className={styles.container}>
+        <iframe
+          width="100%"
+          height="100%"
+          src={video.link}
+          title={video.title}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </section>
+    </>
+  );
+}
