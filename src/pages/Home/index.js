8b784f2667b92f9ca1a160fd4ad38c61b7ba0200
OtavioFSantos <otaviofs01@gmail.com>
2023-01-09 12:34:46 -0300

    testing for vercel deploy error

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,6 @@ import Banner from "components/Banner";
 import Title from "components/Title";
 import Card from "components/Card";
 import movies from "json/db.json";
-import styles from "./Home.module.css";
 
 export default function Home() {
   return (
@@ -11,7 +10,7 @@ export default function Home() {
       <Title>
         <h1>Select the movies you would like to watch!</h1>
       </Title>
-      <section className={styles.container}>
+      <section>
         {movies.map((movie) => {
           return <Card {...movie} key={movie.id} />;
         })}
