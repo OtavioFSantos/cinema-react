c851ccef918c498c06317750dcef0304be236e0e
OtavioFSantos <otaviofs01@gmail.com>
2023-01-09 12:42:40 -0300

    test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,7 @@ import Banner from "components/Banner";
 import Title from "components/Title";
 import Card from "components/Card";
 import movies from "json/db.json";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       <Title>
         <h1>Select the movies you would like to watch!</h1>
       </Title>
-      <section>
+      <section className={styles.container}>
         {movies.map((movie) => {
           return <Card {...movie} key={movie.id} />;
         })}
