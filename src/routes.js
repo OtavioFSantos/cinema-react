7a7168f5cfdd7689be561596b8e76f1b953168c4
OtavioFSantos <otaviofs01@gmail.com>
2023-01-08 23:44:22 -0300

    add route for video player

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Header from "components/Header";
 import Footer from "components/Footer";
 import Container from "components/Container";
 import FavoritesProvider from "Context/Favorites";
+import Player from "pages/Player";
 
 export default function AppRoutes() {
   return (
@@ -15,6 +16,7 @@ export default function AppRoutes() {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/favorites" element={<Favorites />}></Route>
+            <Route path="/:id" element={<Player />}></Route>
           </Routes>
         </FavoritesProvider>
       </Container>
